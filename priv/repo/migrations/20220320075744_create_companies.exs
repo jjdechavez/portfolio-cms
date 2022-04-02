a46d618fb516ba787b333c580ba7b4b836bb5c3a
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2022-04-02 17:25:23 +0800

    Update createCompanies migration - added user_id field to references user

--- priv/repo/migrations/20220320075744_create_companies.exs
+++ priv/repo/migrations/20220320075744_create_companies.exs
@@ -3,11 +3,14 @@ defmodule Portfolio.Repo.Migrations.CreateCompanies do
 
   def change do
     create table(:companies) do
-      add :name, :string
-      add :url_link, :string
-      add :location, :string
+      add(:name, :string)
+      add(:url_link, :string)
+      add(:location, :string)
+      add(:user_id, references(:users, on_delete: :nothing))
 
       timestamps()
     end
+
+    create(index(:companies, [:user_id]))
   end
 end
